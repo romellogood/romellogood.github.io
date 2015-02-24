52852e18450dba52181e73d6e781fd89da470777
Romello Goodman <romellogood@gmail.com>
2015-02-24 00:41:04 -0500

    Added BlogJS Example Site

---
+++ blogjs/posts.js
@@ -0,0 +1,72 @@
+var posts = {
+  0: {
+    "number": 0,
+    "title": "Post 1",
+    "body":
+      '<p>The 1st Post</p> \
+      <hr>',
+  },
+  1: {
+    "number": 1,
+    "title": "Post 2",
+    "body":
+      '<p>The 2nd Post</p> \
+      <hr>',
+  },
+  2: {
+    "number": 2,
+    "title": "Post 3",
+    "body":
+      '<p>The 3rd Post</p> \
+      <hr>',
+  },
+  3: {
+    "number": 3,
+    "title": "Post 4",
+    "body":
+      '<p>The 4th Post</p> \
+      <hr>',
+  },
+  4: {
+    "number": 4,
+    "title": "Post 5",
+    "body":
+      '<p>The 5th Post</p> \
+      <hr>',
+  },
+  5: {
+    "number": 5,
+    "title": "Post 6",
+    "body":
+      '<p>The 6th Post</p> \
+      <hr>',
+  },
+  6: {
+    "number": 6,
+    "title": "Post 7",
+    "body":
+      '<p>The 7th Post</p> \
+      <hr>',
+  },
+  7: {
+    "number": 7,
+    "title": "Post 8",
+    "body":
+      '<p>The 8th Post</p> \
+      <hr>',
+  },
+  8: {
+    "number": 8,
+    "title": "Post 9",
+    "body":
+      '<p>The 9th Post</p> \
+      <hr>',
+  },
+  9: {
+    "number": 9,
+    "title": "Post 10",
+    "body":
+      '<p>The 10th Post</p> \
+      <hr>',
+  },
+}
