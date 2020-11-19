4f0f8f1676bb14bdf2c204b2dde2d135093847b1
Romello Goodman <romellogood@gmail.com>
2020-11-19 17:11:32 -0500

    :wave: goodbye preact, react 17, nextjs 10

--- next.config.js
+++ next.config.js
@@ -3,34 +3,4 @@ module.exports = {
   experimental: {
     modern: true,
   },
-  webpack(config, {dev, isServer}) {
-    /**
-     * preact-next
-     * https://github.com/developit/nextjs-preact-demo
-     */
-    const splitChunks = config.optimization && config.optimization.splitChunks;
-
-    if (splitChunks) {
-      const cacheGroups = splitChunks.cacheGroups;
-      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-      if (cacheGroups.framework) {
-        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-          test: preactModules,
-        });
-        cacheGroups.commons.name = 'framework';
-      } else {
-        cacheGroups.preact = {
-          name: 'commons',
-          chunks: 'all',
-          test: preactModules,
-        };
-      }
-    }
-
-    // Install webpack aliases:
-    const aliases = config.resolve.alias || (config.resolve.alias = {});
-    aliases.react = aliases['react-dom'] = 'preact/compat';
-
-    return config;
-  },
 };
