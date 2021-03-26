186d103082228bf88793d9996ad176b39ca54f12
Romello Goodman <romellogood@gmail.com>
2021-03-26 16:03:22 -0400

    since we dont have runtime js on the page, removing fathom package and instead using a script tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,9 @@
-import * as Fathom from 'fathom-client';
 import Head from 'next/head';
 import React, {useEffect} from 'react';
 import 'styles.css';
 
-// Taken from - https://vercel.com/guides/deploying-nextjs-using-fathom-analytics-with-vercel
-export const useFathom = () => {
-  // const router = useRouter();
-
-  useEffect(() => {
-    // Initialize Fathom when the app loads
-    Fathom.load('XRTIFTNI', {
-      includedDomains: ['romellogoodman.com'],
-    });
-
-    // NOTE: We arent doing any next/link transitions but just in case
-    // function onRouteChangeComplete() {
-    //   Fathom.trackPageview();
-    // }
-    // // Record a pageview when route changes
-    // router.events.on('routeChangeComplete', onRouteChangeComplete);
-
-    // // Unassign event listener
-    // return () => {
-    //   router.events.off('routeChangeComplete', onRouteChangeComplete);
-    // };
-  }, []);
-};
-
 const NextApp = (props) => {
   const {Component, pageProps = {}} = props;
-
-  useFathom();
 
   return (
     <>
@@ -48,6 +21,11 @@ const NextApp = (props) => {
           href="https://fonts.googleapis.com/css?family=Public+Sans:400,700&display=swap"
           rel="stylesheet"
         />
+        <script
+          src="https://cdn.usefathom.com/script.js"
+          data-site="XRTIFTNI"
+          defer
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
