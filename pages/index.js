3c3fb1014282f20af01fcc20b2da3ccfac806eda
Romello Goodman <romellogood@gmail.com>
2021-05-14 15:46:07 -0400

    new links and section titles

--- pages/index.js
+++ pages/index.js
@@ -8,27 +8,18 @@ const hrefOutside = {
 const Page = () => {
   return (
     <main>
-      <h3>
+      <h2>
         hello, my name is Romello (<span className="color"> mello </span>)
         Goodman.
-      </h3>
+      </h2>
       <p>
         I am a creative technologist primarily working with web technologies. By
         day, I work as a senior software engineer at The New York Times. By
         night, I use a number of different tools to explore the intersection of
         creativity and code.
       </p>
-      <h3>around the web</h3>
+      <h2>find me around the web</h2>
       <ul>
-        <li>
-          <a
-            href="https://observablehq.com/@romellogoodman?tab=profile"
-            title="Go to Observable."
-            {...hrefOutside}
-          >
-            <p>coding blog</p>
-          </a>
-        </li>
         <li>
           <a
             href="https://twitter.com/mellogood"
@@ -49,15 +40,6 @@ const Page = () => {
         </li>
         <li>
           <a
-            href="https://github.com/romellogoodman"
-            title="Go to Github."
-            {...hrefOutside}
-          >
-            <p>github</p>
-          </a>
-        </li>
-        <li>
-          <a
             href="https://www.patreon.com/romellogoodman"
             title="Go to Patreon."
             {...hrefOutside}
@@ -65,8 +47,26 @@ const Page = () => {
             <p>patreon</p>
           </a>
         </li>
+        <li>
+          <a
+            href="https://observablehq.com/@romellogoodman?tab=profile"
+            title="Go to Observable."
+            {...hrefOutside}
+          >
+            <p>coding blog</p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/romellogoodman"
+            title="Go to Github."
+            {...hrefOutside}
+          >
+            <p>github</p>
+          </a>
+        </li>
       </ul>
-      <h3>projects</h3>
+      <h2>projects</h2>
       <ul>
         <li>
           <a
@@ -74,7 +74,7 @@ const Page = () => {
             title="Go to Good Graphics."
             {...hrefOutside}
           >
-            <p>good graphics</p>
+            <p>Good Graphics</p>
           </a>
         </li>
         <li>
@@ -87,21 +87,12 @@ const Page = () => {
           </a>
         </li>
       </ul>
-      <h3>things i've written</h3>
+      <h2>articles</h2>
       <ul>
         <li>
           <a
-            href="https://docs.google.com/presentation/d/1bpnRUbXtXVqTJB1k3Y0afpA7v6EATZndRGf_Ja9ggCg/edit#slide=id.p"
-            title="Go to Observable Notebook"
-            {...hrefOutside}
-          >
-            <p>Graphic = fn(state)</p>
-          </a>
-        </li>
-        <li>
-          <a
             href="https://increment.com/remote/code-is-sourdough"
-            title="Go to Increment article"
+            title="Go to the Increment article."
             {...hrefOutside}
           >
             <p>Code is sourdough</p>
@@ -110,7 +101,7 @@ const Page = () => {
         <li>
           <a
             href="https://increment.com/software-architecture/a-monorepo-renaissance"
-            title="Go to Increment article"
+            title="Go to the Increment article."
             {...hrefOutside}
           >
             <p>A monorepo renaissance</p>
@@ -119,10 +110,43 @@ const Page = () => {
         <li>
           <a
             href="https://open.nytimes.com/what-if-we-used-images-to-navigate-new-york-times-content-5a6d87ab3688"
-            title="Go to The Times Open article"
+            title="Go to The Times Open article."
             {...hrefOutside}
           >
             <p>What If We Used Images to Navigate New York Times Content?</p>
+          </a>
+        </li>
+      </ul>
+      <h2>talks and panels</h2>
+      <ul>
+        <li>
+          <a
+            href="https://twitter.com/ReactNorway/status/1392887518471671809"
+            title="Go to announcement tweet."
+            {...hrefOutside}
+          >
+            <p>
+              Urban Planning & Codebase Architecture. Or, how not to end up with
+              a Wendy's in the middle of your codebase
+            </p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://docs.google.com/presentation/d/1bpnRUbXtXVqTJB1k3Y0afpA7v6EATZndRGf_Ja9ggCg/edit#slide=id.p"
+            title="Go to the Google slides presentation."
+            {...hrefOutside}
+          >
+            <p>Graphic = fn(state)</p>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/watch?v=4uLV8ieQVZk"
+            title="Go to the Youtube video."
+            {...hrefOutside}
+          >
+            <p>This Dot Media's React Contributor Days May 2021</p>
           </a>
         </li>
       </ul>
